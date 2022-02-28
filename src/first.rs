96a87e9ab003ca6e807ab3b91ba2de0d6a0d25c8
kingfree <kingfree@toyama.moe>
2022-02-28 11:03:10 +0800

    new

--- src/first.rs
+++ src/first.rs
@@ -1,11 +1,19 @@
-#[derive(Debug)]
-enum List<T> {
-    Cons(T, Box<List<T>>),
-    Nil,
+pub struct List {
+    head: Link,
 }
 
-#[test]
-fn test() {
-    let list: List<i32> = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
-    println!("{:?}", list);
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
 }
